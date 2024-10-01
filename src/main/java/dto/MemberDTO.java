6c3c89677e290359182470b3f0b724ef7d1db65b
Rayane <mustaphagameover111@gmail.com>
2024-10-01 15:28:57 +0100

    creating team and member DTO

--- src/main/java/dto/MemberDTO.java
+++ src/main/java/dto/MemberDTO.java
@@ -1,40 +1,81 @@
 package dto;
 
-import models.Member;
 
-import java.util.Objects;
+import models.enums.Role;
 
 
 public class MemberDTO {
 	
-		private int id;
-		private String name;
+	private int id;
+	private String fname;
+	private String lname;
+	private String email;
+	private Role role;
+	private int team_id;
+	
 
+	
+	public MemberDTO() {
 		
-		public MemberDTO() {
-			
-		}
-		
-		public MemberDTO(int id, String name) {
-			this.id = id;
-			this.name = name;
-		}
-		
-		
-		public int getId() {
-			return id;
-		}
+	}
+	
+	public MemberDTO(int id, String fname, String lname, String email, Role role, int team_id) {
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.role = role;
+		this.team_id = team_id;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
 
-		public void setId(int id) {
-			this.id = id;
-		}
-		
-		public String getName() {
-			return name;
-		}
-		
-		public void setName(String name) {
-			this.name = name;
-		}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFname() {
+		return fname;
+	}
+	
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public int getTeamId() {
+        return team_id;
+    }
+
+    public void setTeamId(int team_id) {
+        this.team_id = team_id;
+    }
 		
 }
