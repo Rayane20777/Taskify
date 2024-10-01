b2abf645cf0d32be2226ea50e749cad9dec19c9f
Rayane <mustaphagameover111@gmail.com>
2024-10-02 00:54:44 +0100

    Member DAO done

--- src/main/java/dao/MemberDAO.java
+++ src/main/java/dao/MemberDAO.java
@@ -1,5 +1,12 @@
 package dao;
 
-public interface MemberDAO {
+import java.util.List;
 
+import models.Member;
+
+public interface MemberDAO {
+    void addMember(Member member);
+    List<Member> getAllMembers();
+    void updateMember(Member member);
+    void deleteMember(int id);
 }
