7d2a156e795b0b64cacbf651dd2835dc2ce490f6
Rayane <mustaphagameover111@gmail.com>
2024-10-02 00:58:23 +0100

    Member Service done

--- src/main/java/services/MemberService.java
+++ src/main/java/services/MemberService.java
@@ -1,5 +1,11 @@
 package services;
 
-public interface MemberService {
+import dto.MemberDTO;
+import java.util.List;
 
+public interface MemberService {
+    void addMember(MemberDTO memberDTO);
+    List<MemberDTO> getAllMembers();
+    void updateMember(MemberDTO memberDTO);
+    void deleteMember(int id);
 }
--- src/main/java/services/MemberServiceImpl.java
+++ src/main/java/services/MemberServiceImpl.java
@@ -1,5 +1,51 @@
 package services;
 
-public class MemberServiceImpl {
+import dao.MemberDAO;
+import dto.MemberDTO;
+import models.Member;
+import repositories.MemberRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberServiceImpl implements MemberService{
+    private MemberDAO memberDAO;
+    private MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberDAO memberDAO, MemberRepository memberRepository) {
+        this.memberDAO = memberDAO;
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void addMember(MemberDTO memberDTO) {
+        Member member = new Member(memberDTO.getId(), memberDTO.getFname(), memberDTO.getLname(),
+                memberDTO.getEmail(), memberDTO.getRole(), memberDTO.getTeamId());
+        memberDAO.addMember(member);
+    }
+    
+    @Override
+    public List<MemberDTO> getAllMembers() {
+        List<Member> members = memberDAO.getAllMembers();
+        List<MemberDTO> memberDTOs = new ArrayList<>();
+        for (Member member : members) {
+            memberDTOs.add(new MemberDTO(member.getId(), member.getFname(), member.getLname(),
+                    member.getEmail(), member.getRole(), member.getTeamId()));
+        }
+        return memberDTOs;
+    }
+    
+    @Override
+    public void updateMember(MemberDTO memberDTO) {
+        Member member = new Member(memberDTO.getId(), memberDTO.getFname(), memberDTO.getLname(),
+                memberDTO.getEmail(), memberDTO.getRole(), memberDTO.getTeamId());
+        memberDAO.updateMember(member);
+    }
+
+    @Override
+    public void deleteMember(int id) {
+        memberDAO.deleteMember(id);
+    }
+
 
 }
